c9b6f09de9662bc63b5fa87a785177b2f2513215
saurav5121998 <saurav5121998@gmail.com>
2026-03-29 15:45:41 +0000

    fix: format code

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,8 +1,10 @@
-import pytest
-from fastapi.testclient import TestClient
-import sys, os
+import os
+import sys
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
+from fastapi.testclient import TestClient
+
 from main import app
 
 client = TestClient(app)
